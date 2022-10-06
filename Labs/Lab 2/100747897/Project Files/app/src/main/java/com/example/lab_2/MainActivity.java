4d396ef1c3458c83c397b407107ba742ce9a64ba
airwick989 <73313597+airwick989@users.noreply.github.com>
2022-10-05 22:13:04 -0400

    Added new lab files

---
+++ Labs/Lab 2/100747897/Project Files/app/src/main/java/com/example/lab_2/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.lab_2;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnOpenCam;
+    ImageView ivPicture;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnOpenCam = findViewById(R.id.btnOpenCam);
+        ivPicture = findViewById(R.id.ivPicture);
+
+        btnOpenCam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    @Override
+    protected  void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+        ivPicture.setImageBitmap(bitmap);
+    }
+}
